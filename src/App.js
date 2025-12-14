a4620f42a8e5725b49ddbc3b7218d4b7c70c70ff
namnmm <namtt2566@gmail.com>
2025-12-15 06:04:12 +0700

    99

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
   const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
   const storedTheme = useSelector((state) => state.theme)
 
+
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.href.split('?')[1])
     const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
